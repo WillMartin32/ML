36686dbc1ec36b61596f556e16531182d6b30d63
William Martin <you@example.com>
2021-11-15 15:56:53 -0600

    worked on quiz

--- Quiz2.py
+++ Quiz2.py
@@ -2,25 +2,20 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LinearRegression
 
-test = pd.read_csv("gameratings.csv")
-target = pd.read_csv("test_esrb.csv")
+train = pd.read_csv("gameratings.csv")
+test = pd.read_csv("test_esrb.csv")
+
+#34 columns
+
+x_train = train.values.reshape(-1,33)
+
+y_train = train.target.values.reshape(-1,1)
 
 
-lr = LinearRegression()
+knn = KNeighborsClassifier()
 
-lr.fit(X=x_train, y=y_train)
+knn.fit(X=x_train ,y=y_train)
 
-coef = lr.coef_
-intercept = lr.intercept_
-
-predicted = lr.predict(x_test)
-expected =  y_test
-
-
-#knn = KNeighborsClassifier()
-
-#knn.fit(X=test ,y=target)
-
-
+print('Done')
 
 
