dc5990bd56a5fb55d3ec103a748715330e46a377
William Martin <you@example.com>
2021-11-16 22:56:06 -0600

    Finished Quiz

--- Quiz2.py
+++ Quiz2.py
@@ -1,21 +1,55 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LinearRegression
 
 train = pd.read_csv("gameratings.csv")
 test = pd.read_csv("test_esrb.csv")
+names = pd.read_csv("target_names.csv")
 
-#34 columns
 
-x_train = train.values.reshape(-1,33)
+x_train = train.loc[0:1894, 'console':'violence']
+y_train = train.loc[0:1894, 'Target']
 
-y_train = train.target.values.reshape(-1,1)
+x_test = test.loc[0:1894, 'console':'violence']
+y_test = test.loc[0:1894, 'Target']
 
 
 knn = KNeighborsClassifier()
-
 knn.fit(X=x_train ,y=y_train)
 
-print('Done')
+predicted = knn.predict(X=x_test)
+expected = y_test
+
+#print(predicted[:20])
+#print(expected[:20])
+
+dict = {}
+y = 0
+
+for x in names.T:
+    target_class = names.target_class.values[y]
+    target_name = names.target_name.values[y]
+    y += 1
+    dict[target_class] = target_name
+
+#print(dict)
+
+predicted = [dict[x] for x in predicted]
+expected = [dict[x] for x in expected]
+
+#print(predicted[:20])
+#print(expected[:20])
+
+wrong = [(p,e) for (p,e) in zip(predicted, expected) if p != e]
+
+print(wrong)
 
 
+outfile = open("Quiz2Predictions.csv", 'w')
+outfile.write('title,prediction\n')
+
+y = 0
+for x in test.T:
+  outfile.write(test.values[y][0] + ',' + predicted[y] + '\n')
+  y += 1
+
+outfile.close()
